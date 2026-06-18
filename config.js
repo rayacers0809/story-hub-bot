1d24fdf2dab9ecace2a4b7df0323d2cd2468e520
rayacers0809 <66523337+rayacers0809@users.noreply.github.com>
2026-06-19 02:35:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ module.exports = {
       label: '스태프 지원',
       description: '스태프 지원 문의는 이 티켓을 이용해 주세요',
       value: 'SERVER',
-      emoji: '⭐',
+      emoji: '💼',
       channelPrefix: '스태프지원',
     },
   ],
